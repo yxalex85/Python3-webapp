00832b0dd084a57dfdea4a928143db1e4fe2c65f
AlexYx85 <yxalex@gmail.com>
2017-10-26 15:35:35 +0800

    create sql pool

--- www/orm.py
+++ www/orm.py
@@ -9,6 +9,13 @@ async def create_pool(loop, **kw):
     global __pool
     __pool = await aiomysql.create_pool(
         host = kw.get('host', 'localhost'),
-        port = kw.get('port', 3306)
-
+        port = kw.get('port', 3306),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset', 'utf8'),
+        autocommit = kw.get('autocommit', True),
+        maxsize = kw.get('maxsize', 10),
+        minsize = kw.get('minsize', 1),
+        loop = loop
     )
